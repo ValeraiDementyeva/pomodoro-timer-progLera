ec6139fc7fbf48ed93a8a08f4174d2e49d08fdfb
Valerai Dementyeva <lera.dementjeva@yandex.ru>
2022-05-22 15:39:52 +0300

    Create lables and buttons.

--- pomodoro timer progLera/ViewController.swift
+++ pomodoro timer progLera/ViewController.swift
@@ -9,11 +9,30 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var timeLableWork: UILabel!
+
+    @IBOutlet weak var startButtonWork: UIButton!
+
+    @IBOutlet weak var timeLableRelax: UILabel!
+
+    @IBOutlet weak var startButtonRelax: UIButton!
+
+    @IBOutlet weak var cancelButton: UIButton!
+
+
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
+    @IBAction func startButtonTapped(_ sender: Any) {
+    }
 
+    @IBAction func startButtomRelax(_ sender: Any) {
+    }
+
+    @IBAction func cancelTimetButton(_ sender: Any) {
+    }
+    
 }
 
